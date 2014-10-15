f0d91f3ee82467d06e19ca7ddad17a92520b421f
Sam Brown <samdanielbrown@gmail.com>
2014-10-15 23:46:53 +0100

    second commit - less bugs, more polished

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 recon-ng-bt_lookup
 ==================
 
-A BT lookup module for recon-ng
+A BT phonebook lookup module for recon-ng - place in any folder under the modules directory in recon-ng and it will be usable.
+Takes user details from the contacts table, adds phone_number and address columns and then tries
+to add them to each record - prompting the user to choose when there are multiple results.
+
+Based off @catalyst256 bt_lookup code.
--- bt_lookup.py
+++ bt_lookup.py
@@ -5,64 +5,70 @@ from BeautifulSoup import BeautifulSoup
 class Module(module.Module):
 
     def __init__(self, params):
-        module.Module.__init__(self, params, query='SELECT last_name,first_name,region FROM contacts WHERE last_name IS NOT NULL and country ="United Kingdom" ORDER BY last_name')
+        module.Module.__init__(self, params, query='SELECT first_name,last_name,region FROM contacts WHERE last_name IS NOT NULL and country ="United Kingdom" ORDER BY last_name')
+        try:
+            self.query('ALTER TABLE contacts ADD COLUMN address TEXT')
+            self.query('ALTER TABLE contacts ADD COLUMN phone_number TEXT')
+        except:
+            pass
         self.info = {
-                     'Name': 'BT Lookup',
-                     'Author': 'Adam Maxwell (@catalyst265) and Sam Brown (@samdb)',
-                     'Description': 'Harvests contacts from Facebook.com. Updates the \'contacts\' table with the results.',
-                     }
+            'Name': 'BT Lookup',
+            'Author': 'Adam Maxwell (@catalyst265) and Sam Brown (@samdb)',
+            'Description': """Harvests phone numbers and addresses for UK contacts from the 
+            BT Phonebook website. Updates the \'contacts\' table with the results."""
+        }
 
-    def module_run(self, details):
-        count = 0
-        for person in details:
+    def module_run(self, contacts):
+        self.updated_count = 0
+        for person in contacts:
             url = 'http://www.thephonebook.bt.com/publisha.content/en/search/residential/search.publisha'
-            surname = person[0]
-            location = person[2]
-            first_name = person[1]
+            first_name,last_name,region = person
             first_initial = first_name[0]
-            payload = {'Surname':surname,
-                        'Location':location,
+            payload = {'Surname':last_name,
+                        'Location':region,
                         'Initial':first_initial,
                         'Street':''
                        }
             resp = self.request(url,payload=payload)
             parsed_html = BeautifulSoup(resp.text)
             x = parsed_html.body.findAll('div', attrs={'class':'recordBody'})
-            if len(x) > 0:
-                try:
-                    self.query('ALTER TABLE contacts ADD COLUMN address TEXT')
-                    self.query('ALTER TABLE contacts ADD COLUMN phone_number TEXT')
-                except:
-                    #already added...should probably make this more eloquent...
-                    pass
-            if len(x) == 1:
-                wut = s.text.replace('Tel: (', ',').replace(')', '').replace('-Text Number', ',').replace('-Map', '').split(',')
-                phone_number = wut[1]
-                address = wut[2] + ',' + wut[3] + ',' + wut[4]
-                self.output("Updated contact with first_name:%s, last_name:%s and region: %s to have phone_number:%s and address:%s")
-                self.query('UPDATE contacts SET phone_number=?, address=? WHERE last_name =? AND region=? AND first_name LIKE ?',(phone_number,address,surname,location,first_initial +'%'))
+            parsed_results = []
+            for result in x:
+                lookup_result = result.text.replace('Tel: (', ',').replace(')', '').replace('-Text Number', ',').replace('-Map', '').split(',')
+                names = lookup_result[0].split(' ')
+                #Only some results have full first names - if a result does a full first name make sure 
+                #it matches the first_name that we have for the contact
+                if len(names[0]) == 1 or (len(names[0]) > 1 and names[0] == first_name):
+                    parsed_results.append(lookup_result)
+            #if no results skip on to next person
+            if len(parsed_results) == 0:
+                continue
+            #if only one result, assume its who we want
+            if len(parsed_results) == 1:
+                self.update_contacts(first_name,last_name,region,parsed_results[0])
             else:
-                self.output("Choose option (enter non-integer to skip) to set phone_number and address of %s %s who lives in region: %s to:" % (first_name,surname,location))
-                for i,s in enumerate(x):
-                    wut = s.text.replace('Tel: (', ',').replace(')', '').replace('-Text Number', ',').replace('-Map', '').split(',')
-                    names = wut[0].split(' ')
-                    if len(names[0]) == 1 or (len(names[0]) > 1 and names[0] == first_name):
-                        phone_number = wut[1]
-                        address = wut[2] + ',' + wut[3] + ',' + wut[4]
-                        self.output("%d: phone_number:%s and address:%s" % (i,phone_number,address))
+                self.output("Choose option (enter non-integer to skip) to set phone_number and address of %s %s who lives in region: %s to:" % (first_name,last_name,region))
+                for i,s in enumerate(parsed_results):
+                    phone_number = s[1]
+                    address = ','.join(s[2:])
+                    self.output("%d: phone_number: %s and address: %s" % (i,phone_number,address))
                 while True:
                     choice = raw_input()
                     try:
                         choice = int(choice)
                         try:
-                            chosen = x[choice]
-                            chosen = chosen.text.replace('Tel: (', ',').replace(')', '').replace('-Text Number', ',').replace('-Map', '').split(',')
-                            phone_number = chosen[1]
-                            address = chosen[2] + ',' + chosen[3] + ',' + chosen[4]
-                            self.query('UPDATE contacts SET phone_number=?, address=? WHERE last_name =? AND region=? AND first_name LIKE ?',(phone_number,address,surname,location,first_initial +'%'))
+                            chosen = parsed_results[choice]
+                            self.update_contacts(first_name,last_name,region,chosen)
                             break
                         except:
-                            self.output('invalid choice please re-enter:')
+                            self.output('Invalid choice please re-enter:')
                     except:
                         break
-        count += 1
+        self.output('Added details for %d contact records.' % self.updated_count)     
+
+    def update_contacts(self,first_name,last_name,region,result):
+        phone_number = result[1]
+        address = ','.join(result[2:])
+        self.query('UPDATE contacts SET phone_number=?, address=? WHERE last_name =? AND region=? AND first_name=?',(phone_number,address,last_name,region,first_name))
+        self.output('Updated record with first_name: %s , last_name: %s and region: %s to have phone_number: %s and address: %s.' % (first_name, last_name,region,phone_number,address))
+        self.updated_count += 1
